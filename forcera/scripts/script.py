d0595d26e1bf8b29e85504a4bc897389f0403495
Francisco Valente <famvpereira@protonmail.com>
2023-10-26 19:21:02 +0100

    Modificações
    + Organização do ficheiro .ipynb
    + Criação de uma função que retorna o preço contratual dado o id do
      contrato
    + Criação de uma função que retorna todos os ids da tabela

--- forcera/scripts/script.py
+++ forcera/scripts/script.py
@@ -1,40 +1,55 @@
-import os
-os.chdir("/home/francisco/MECAD/COMP/comp")
-
-
-
+# ---------------------------------------# 
+#              Bibliotecas
+# ---------------------------------------# 
 import psycopg2
 from psycopg2 import OperationalError
+import pandas as pd
+import numpy as np
+from IPython.display import HTML
 
+
+# ---------------------------------------# 
+#      Criar ligação com a database
+# ---------------------------------------# 
 conn = psycopg2.connect(
     host="localhost",
     port = 5434,
     database="aim4ps",
     user="myuser",
     password="1234")
-
-
-
-cur = conn.cursor()  
-# execute a statement"public".
-#cur.execute('SELECT * from "public"."CONTRACTS" LIMIT 10;')
-
-# display the PostgreSQL database server version
-#db_version = cur.fetchone()
-#print(db_version)
-
-
-cur.execute('''
-    SELECT COUNT(*) FROM "public"."CONTRACTS";
-''')
-
-print("Número de contratos : ", cur.fetchone())
-
 cur = conn.cursor()
-cur.execute('''
-    SELECT tipo_procedimento, COUNT (tipo_procedimento)
-    FROM "CONTRACTS"
-    GROUP BY "tipo_procedimento";
-''')
 
-(cur.fetchall())
+
+# ---------------------------------------# 
+#               Funções
+# ---------------------------------------# 
+
+def preco_contrato(id_anuncio):
+    '''
+    Função que retorna preço contratual a partir do id do anúncio
+    '''
+    cur = conn.cursor()
+    cur.execute('''
+        SELECT preco_contratual
+        FROM "CONTRACTS"
+        WHERE idcontrato = %s; ''', (id_anuncio,))
+    return np.asarray(cur.fetchall())[0]
+
+#print(preco_contrato('8584389')) 
+
+
+
+def all_ids():
+    '''
+    Função que retorna todos os ids dos contratos
+    '''
+    cur = conn.cursor()
+    cur.execute(''' 
+                SELECT idcontrato
+                FROM "CONTRACTS";
+                ''')
+    return np.asarray(cur.fetchall())
+
+ids = all_ids()
+print(ids)
+
