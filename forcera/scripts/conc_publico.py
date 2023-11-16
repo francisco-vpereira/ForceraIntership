c900c20c6bbe52c065a37bd4209c973eb4172ac5
Francisco Valente <famvpereira@protonmail.com>
2023-11-16 13:10:31 +0000

    atualização

--- forcera/scripts/conc_publico.py
+++ forcera/scripts/conc_publico.py
@@ -3,4 +3,22 @@ from flags import *
 from functions import *
 
 cpub = cpv('72', 'Concurso público')
-print(cpub)
+
+# Preço Base e Preço Contratual de todos os contratos públicos realizados para CPV's começados por 72
+pb = preco_base3(cpub)
+pc = preco_contrato3(cpub)
+
+df = contratos(cpub).iloc[:,[2,18]]
+df = df.rename(columns={2: 'PrecoBase', 18:'PrecoContratual'})
+df = df.astype('float')
+
+plt.figure(figsize=(20,9))
+sns.barplot(x = df.index, y = df.PrecoBase, color = 'skyblue')
+sns.barplot(x = df.index, y = df.PrecoContratual, color = 'lightgray', fill=0.1)
+plt.ylabel('Preço Contratual / Base')
+plt.show()
+
+
+print(np.where(pb/pc > 3800), pb[91], pc[91])
+
+HTML(contrato(cpub[91]).to_html(index=False))
