4c5dc52ac535d5d9f880757fa9ae5a4f1277dd3d
Francisco Valente <famvpereira@protonmail.com>
2024-02-14 14:13:12 +0000

    Atualiza tabela temporaria com IDs do dia anterior e calcula flags binárias

--- forcera/scripts/cron_jobs/daily_ids.py
+++ forcera/scripts/cron_jobs/daily_ids.py
@@ -32,9 +32,9 @@ conn = psycopg2.connect(
 yesterday = datetime.now() - timedelta(1)
 ontem = datetime.strftime(yesterday, '%Y-%m-%d')
 
+print(ontem)
 
-
-# Obter IDs de concursos publicos de ontem
+# # Obter IDs de concursos publicos de ontem
 cur = conn.cursor()
 cur.execute('''
             SELECT id
@@ -45,10 +45,94 @@ ids_ontem = cur.fetchall()
 
 
 
-
 # Escrever valores na tabela
 cur = conn.cursor()
 for i in ids_ontem:
-    cur.execute('''INSERT INTO table_temp(id, data) VALUES (%s, %s);''', (i,ontem)) 
-
+    cur.execute('''INSERT INTO table_temp(id, data_publicacao, "R003", "R018", "RF2", "RF3") VALUES (%s, %s, false, false, false, false);''', (i,ontem)) 
 conn.commit() 
+
+
+
+
+# Obter IDs de ontem referentes à flag R003
+cur = conn.cursor()
+cur.execute('''
+            UPDATE table_temp
+            SET "R003" = true
+            WHERE table_temp."id" IN (
+                SELECT contratos_basegov."id" 
+                FROM contratos_basegov
+                JOIN concursos_publicos ON contratos_basegov."id" = concursos_publicos."id"
+                WHERE data_publicacao = %s AND 
+                    (concursos_publicos."tipo_contrato" = 'Empreitadas' AND contratos_basegov."anuncio_proposalDeadline" < 14) 
+                    OR
+                    (concursos_publicos."tipo_contrato" = 'Bens e Serviços' AND contratos_basegov."anuncio_proposalDeadline" < 6)
+                );
+            ''', (ontem,))
+conn.commit() 
+
+
+
+
+# Obter IDs de ontem referentes à flag R018
+cur = conn.cursor()
+cur.execute('''
+            UPDATE table_temp
+            SET "R018" = true
+            WHERE table_temp."id" IN (
+                SELECT contratos_basegov."id"
+                FROM contratos_basegov
+                JOIN concursos_publicos ON contratos_basegov."id" = concursos_publicos."id"
+                WHERE data_publicacao = %s AND concursos_publicos."nr_entidadesconcorrentes" = 1
+	        );''', (ontem,))
+conn.commit() 
+
+
+
+
+
+# Obter IDs de ontem referentes à flag RF2
+cur = conn.cursor()
+cur.execute('''
+            UPDATE table_temp
+            SET "RF2" = true
+            WHERE table_temp."id" IN (
+                SELECT contratos_basegov."id" 
+                FROM contratos_basegov
+                WHERE data_publicacao = %s AND tipo_procedimento = 'Concurso público' 
+                    AND TO_CHAR(contratos_basegov."anuncio_drPublicationDate", 'MM-DD') 
+                    IN ('01-01', '04-25', '05-01', '06-10', '08-15','10-05', '11-01', '12-01', '12-08', '12-25')
+                );''', (ontem,))
+conn.commit() 
+
+
+
+
+
+# Obter IDs de ontem referentes à flag RF3
+cur = conn.cursor()
+cur.execute('''
+            UPDATE table_temp
+            SET "RF3" = true
+            WHERE table_temp."id" IN (
+            SELECT contratos_basegov."id" 
+                FROM contratos_basegov 
+                WHERE data_publicacao = %s AND contratos_basegov."totalEffectivePrice" > 0 AND ABS(contratos_basegov."totalEffectivePrice" - preco_contratual) > 0
+                );
+            ''', (ontem,))
+conn.commit() 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
