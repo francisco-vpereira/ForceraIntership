cd2616ae6dab247c945be337842d1a1e3124f22d
Francisco Valente <famvpereira@protonmail.com>
2024-03-01 11:44:36 +0000

    Mecanismo que começa a funcionar a partir do último ID gravada

--- forcera/scripts/cron_jobs/daily_ids.py
+++ forcera/scripts/cron_jobs/daily_ids.py
@@ -1,76 +1,103 @@
+# Packages
 import psycopg2
 from psycopg2 import OperationalError
 from psycopg2 import sql
+
 import pandas as pd
 import numpy as np
 import matplotlib.pylab as plt
-from scipy.optimize import curve_fit
 import seaborn as sns
-from IPython.display import HTML
-import webbrowser
-from collections import defaultdict
-import networkx as nx
-import csv
-import os
+
 from datetime import datetime, timedelta
 
+import os
+from dotenv import load_dotenv
+dotenv_path = os.path.expanduser('~/.pwds.env')
+load_dotenv(dotenv_path)
 
 
 
-# LIGAÇÃO À BASE DE DADOS
+# Conexão à base de dados
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT')
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+
+
 conn = psycopg2.connect(
-    host = "contratos-base-gov1.cf87yxnqgph8.eu-central-1.rds.amazonaws.com",
-    port = 5432,
-    #database = "contratosbasegov",
-    user = "contratosbasegov",
-    password = "8n9nyeTBFUyCcLJShNrZdPUai2KQkue4")
+                host = db_host,
+                port = db_port,
+                user = db_user,
+                password = db_password
+        )
 
 
 
-# Definir dia de ontem no formato YYYY-MM-DD
-yesterday = datetime.now() - timedelta(1)
-ontem = datetime.strftime(yesterday, '%Y-%m-%d')
+# -------------------------------------------------------------------------------------------------------------------- #
+# ------------------------------------------ UPDATE DA TABELA AUXILIAR ----------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------------- #
 
 
-# Obter valoes das colunas pertecentes à tabela "concursos_publicos" de concursos publicos referentes ao dia de ontem
+# Obter o último ID copiado para a tabela concursos_publicos
 cur = conn.cursor()
 cur.execute('''
-    SELECT
-        contratos_basegov."id",
-        contratos_basegov."data_publicacao",
-        contratos_basegov."contractTypes",
-        contratos_basegov.fundamentacao,
-        contratos_basegov.entidade_adjudicante,
-        contratos_basegov.entidades_contratadas,
-        contratos_basegov.entidades_concorrentes,
-        contratos_basegov."executionPlace"
-    FROM contratos_basegov
-    WHERE data_publicacao = %s AND tipo_procedimento = 'Concurso público';
-''', (ontem,))
+            SELECT
+                concursos_publicos."id"
+            FROM concursos_publicos
+            ORDER BY data_publicacao DESC, concursos_publicos."id" DESC
+            LIMIT 1;
+            ''')
+last_id = cur.fetchone()[0]
 
-ids_ontem = cur.fetchall()
+
+
+# Copiar dados dos novos contratos adicionados à tabela contratos_basegov 
+# Dados são copiados desde o contrato com o ID mais recente até ao contrato com o 
+# ID mais recente da tabela auxiliar concursos_publicos
+cur = conn.cursor()
+cur.execute('''
+            SELECT
+                contratos_basegov."id",
+                contratos_basegov."data_publicacao",
+                contratos_basegov."contractTypes",
+                contratos_basegov.fundamentacao,
+                contratos_basegov.entidade_adjudicante,
+                contratos_basegov.entidades_contratadas,
+                contratos_basegov.entidades_concorrentes,
+                contratos_basegov."executionPlace"
+            FROM contratos_basegov
+            WHERE tipo_procedimento = 'Concurso público' AND contratos_basegov."id" > %s
+            ORDER BY data_publicacao DESC, id DESC;
+            ''', (last_id,))
+new_data = cur.fetchall()
+
+
 
 
 # Inserir os valores extraidos na query anterior na tabela "concursos_publicos"
 cur = conn.cursor()
-for i in ids_ontem:
+for i in new_data:
     cur.execute('''
-        INSERT INTO concursos_publicos(
-            id,
-            data_publicacao,
-            "contractTypes",
-            fundamentacao,
-            entidade_adjudicante,
-            entidades_contratadas,
-            entidades_concorrentes,
-            "executionPlace"
-        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
-    ''', i)
-
+                INSERT INTO concursos_publicos(
+                    id,
+                    data_publicacao,
+                    "contractTypes",
+                    fundamentacao,
+                    entidade_adjudicante,
+                    entidades_contratadas,
+                    entidades_concorrentes,
+                    "executionPlace"
+                ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
+            ''', i)
 conn.commit()
 
 
-# Dar update às colunas auxiliares da tabela concursos_publicos
+
+
+# Atualizar colunas auxiliares da tabela concursos_publicos apenas para os novos IDs
+# 1. Separar entidade_adjudicante em nome da entidade, NIF e URL
+# 2. Mesmo procedimento para a coluna entidades_contratadas
+# 3. Contar o número de entidades concorrentes
 cur = conn.cursor()
 cur.execute('''
             UPDATE concursos_publicos
@@ -82,39 +109,45 @@ cur.execute('''
                 nif2 = split_part(split_part(entidades_contratadas, '(', 2), ')', 1),
                 url2 = split_part(entidades_contratadas, ')(', 2),
                 nr_entidadesconcorrentes = CARDINALITY(STRING_TO_ARRAY(entidades_concorrentes, '|||')) + 1
-            WHERE data_publicacao = %s;
-            ''', (ontem,))
+            WHERE concursos_publicos."id" > %s;
+            ''', (last_id,))
 conn.commit()
 
 
 
+
+# Substituir entradas None por 1 na coluna nr_entidadesconcorrentes
 cur = conn.cursor()
 cur.execute('''
             UPDATE concursos_publicos
             SET nr_entidadesconcorrentes = COALESCE(nr_entidadesconcorrentes, 1)
-            WHERE data_publicacao = %s;
-            ''', (ontem,))
+            WHERE concursos_publicos."id" > %s;
+            ''', (last_id,))
 conn.commit()
 
 
 
 
-
-
+# Caracterização dos contratos de acordo com o tipo de contrato : Empreitadas OU Bens e Serviços
+# Guardar conjunto de IDs mais recentes e respetivos tipos de contrato da tabela contratos_basegov
 cur = conn.cursor()
 cur.execute('''
             SELECT id, concursos_publicos."contractTypes"
             FROM concursos_publicos
-            WHERE data_publicacao = %s;
-            ''', (ontem,))
-
+            WHERE concursos_publicos."id" > %s
+            ORDER BY data_publicacao DESC, id DESC;
+            ''', (last_id,))
 tcs = cur.fetchall()
 
 
 
+
+# Caracterizar cada um dos novos IDs relativamente ao tipo de contrato
+# Percorrer cada um dos IDs mais recentes
+# i[0] = ID
+# i[1] = tipo de contrato
 cur = conn.cursor()
 for i in tcs:
-
     if 'obras' in i[1]:
         cur.execute('''
                     UPDATE concursos_publicos
@@ -136,27 +169,40 @@ for i in tcs:
 
 
 
+# -------------------------------------------------------------------------------------------------------------------- #
+# ------------------------------------------------- CÁLCULO DAS FLAGS  ----------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------------- #
 
+
+# Guardar conjunto de IDs mais recentes da tabela contratos_basegov desde o último ID analisado
 cur = conn.cursor()
 cur.execute('''
-    SELECT contratos_basegov."id"
-    FROM contratos_basegov
-    WHERE data_publicacao = %s AND tipo_procedimento = 'Concurso público';
-''', (ontem,))
+            SELECT
+                contratos_basegov."id",
+                contratos_basegov."data_publicacao"
+            FROM contratos_basegov
+            WHERE tipo_procedimento = 'Concurso público' AND contratos_basegov."id" > %s
+            ORDER BY data_publicacao DESC, id DESC;;
+            ''', (last_id,))
+new_ids = cur.fetchall()
 
-idsontem = cur.fetchall()
+
 
 
 # Escrever valores dos IDs de ontem na tabela
-# Inicialmente, definir o valor das flags binárias como false. Serão atualizadas nos passos seguintes
+# Inicialmente, definir o valor das flags binárias como false
+# Nos passos seguintes, os contratos com flag ativada da tabela table_temp serão atualizados
 cur = conn.cursor()
-for i in idsontem:
-    cur.execute('''INSERT INTO table_temp(id, data_publicacao, "R003", "R018", "RF2", "RF3") VALUES (%s, %s, false, false, false, false);''', (i,ontem)) 
+for i,j in new_ids:
+    cur.execute(''' INSERT INTO table_temp(id, data_publicacao, "R003", "R018", "RF2", "RF3") 
+                    VALUES (%s, %s, false, false, false, false)
+                ;''', (i,j)) 
 conn.commit() 
 
 
 
-# Obter IDs de ontem referentes à flag R003 e dar update na tabela
+
+# Aplicar flag R003 ao novo conjunto de IDs 
 cur = conn.cursor()
 cur.execute('''
             UPDATE table_temp
@@ -165,18 +211,18 @@ cur.execute('''
                 SELECT contratos_basegov."id" 
                 FROM contratos_basegov
                 JOIN concursos_publicos ON contratos_basegov."id" = concursos_publicos."id"
-                WHERE contratos_basegov."data_publicacao" = %s AND 
+                WHERE contratos_basegov."id" > %s AND 
                     (concursos_publicos."tipo_contrato" = 'Empreitadas' AND contratos_basegov."anuncio_proposalDeadline" < 14) 
                     OR
                     (concursos_publicos."tipo_contrato" = 'Bens e Serviços' AND contratos_basegov."anuncio_proposalDeadline" < 6)
-                );
-            ''', (ontem,))
+            );
+            ''', (last_id,))
 conn.commit() 
 
 
 
 
-# # Obter IDs de ontem referentes à flag R018 e dar update na tabela
+# Aplicar flag R018 ao novo conjunto de IDs 
 cur = conn.cursor()
 cur.execute('''
             UPDATE table_temp
@@ -185,15 +231,15 @@ cur.execute('''
                 SELECT contratos_basegov."id"
                 FROM contratos_basegov
                 JOIN concursos_publicos ON contratos_basegov."id" = concursos_publicos."id"
-                WHERE contratos_basegov."data_publicacao" = %s AND concursos_publicos."nr_entidadesconcorrentes" = 1
-	        );''', (ontem,))
+                WHERE contratos_basegov."id" > %s AND concursos_publicos."nr_entidadesconcorrentes" = 1
+	        );''', (last_id,))
 conn.commit() 
 
 
 
 
 
-# # Obter IDs de ontem referentes à flag RF2 e dar update na tabela
+# Aplicar flag RF2 ao novo conjunto de IDs 
 cur = conn.cursor()
 cur.execute('''
             UPDATE table_temp
@@ -201,17 +247,16 @@ cur.execute('''
             WHERE table_temp."id" IN (
                 SELECT contratos_basegov."id" 
                 FROM contratos_basegov
-                WHERE contratos_basegov."data_publicacao" = %s AND tipo_procedimento = 'Concurso público' 
+                WHERE contratos_basegov."id" > %s AND tipo_procedimento = 'Concurso público' 
                     AND TO_CHAR(contratos_basegov."anuncio_drPublicationDate", 'MM-DD') 
                     IN ('01-01', '04-25', '05-01', '06-10', '08-15','10-05', '11-01', '12-01', '12-08', '12-25')
-                );''', (ontem,))
+            );''', (last_id,))
 conn.commit() 
 
 
 
 
-
-# # Obter IDs de ontem referentes à flag RF3 e dar update na tabela
+# Aplicar flag RF3 ao novo conjunto de IDs 
 cur = conn.cursor()
 cur.execute('''
             UPDATE table_temp
@@ -219,7 +264,7 @@ cur.execute('''
             WHERE table_temp."id" IN (
             SELECT contratos_basegov."id" 
                 FROM contratos_basegov 
-                WHERE contratos_basegov."data_publicacao" = %s AND contratos_basegov."totalEffectivePrice" > 0 AND ABS(contratos_basegov."totalEffectivePrice" - preco_contratual) > 0
+                WHERE contratos_basegov."id" > %s AND contratos_basegov."totalEffectivePrice" > 0 AND ABS(contratos_basegov."totalEffectivePrice" - preco_contratual) > 0
                 );
-            ''', (ontem,))
+            ''', (last_id,))
 conn.commit() 
